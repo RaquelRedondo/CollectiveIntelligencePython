e825f54075bf148f67a75acf6b6f7bd64efb64bc
RaquelRedondo <raquelro22@gmail.com>
2016-06-04 03:22:25 +0100

    Adding extra functions to recommendations.py

--- recommendations.py
+++ recommendations.py
@@ -3,23 +3,43 @@
 
 from math import sqrt
 
-critics = {'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane' : 3.5, 'Just My Luck' : 3.0 , 'Superman Returns' : 3.5, 'You, Me and Dupree' : 2.5, 'The Night Listener': 3.0}, 'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5,  'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0, 'You, Me and Dupree': 3.5}, 'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0, 'Superman Returns': 3.5, 'The Night Listener': 4.0}, 'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0, 'The Night Listener': 4.5, 'Superman Returns': 4.0, 'You, Me and Dupree': 2.5}, 'Mick LaSalle': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0, 'You, Me and Dupree': 2.0}, 'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5}, 'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}}
+# A dictionary of movie critics and their ratings of a small
+# set of movies
+critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
+'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5,
+'The Night Listener': 3.0},
+'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5,
+'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0,
+'You, Me and Dupree': 3.5},
+'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,
+'Superman Returns': 3.5, 'The Night Listener': 4.0},
+'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
+'The Night Listener': 4.5, 'Superman Returns': 4.0,
+'You, Me and Dupree': 2.5},
+'Mick LaSalle': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
+'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0,
+'You, Me and Dupree': 2.0},
+'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
+'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
+'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}}
 
 
 # Returns a distance-based similarity score for person1 and person2
-def sim_distance(prefs,person1,person2):
+def sim_distance(prefs, person1, person2):
     # Get the list of shared_items
-    si={}
+    si = {}
     for item in prefs[person1]:
         if item in prefs[person2]:
-            si[item]=1
-# if they have no ratings in common, return 0
+            si[item] = 1
+        # if they have no ratings in common, return 0
     if len(si) == 0:
         return 0
-# Add up the squares of all the differences
-    sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item], 2) for item in prefs[person1] if item in prefs[person2]])
+    # Add up the squares of all the differences
+    sum_of_squares = sum(
+        [pow(prefs[person1][item] - prefs[person2][item], 2) for item in prefs[person1] if item in prefs[person2]])
 
-    return 1/(1+sum_of_squares)
+    return 1 / (1 + sum_of_squares)
+
 
 def sim_pearson(prefs, p1, p2):
     # Get the list of the mutually rated items
@@ -43,13 +63,71 @@ def sim_pearson(prefs, p1, p2):
     sum2Sq = sum([pow(prefs[p2][it], 2) for it in si])
 
     # Sum up the products
-    pSum = sum([prefs[p1][it]*prefs[p2][it] for it in si])
+    pSum = sum([prefs[p1][it] * prefs[p2][it] for it in si])
 
     # Calculate the Pearson score
-    num = pSum-(sum1*sum2/n)
-    den = sqrt((sum1Sq-pow(sum1, 2)/n) * (sum2Sq-pow(sum2, 2)/n))
+    num = pSum - (sum1 * sum2 / n)
+    den = sqrt((sum1Sq - pow(sum1, 2) / n) * (sum2Sq - pow(sum2, 2) / n))
     if den == 0: return 0
 
-    r = num/den
+    r = num / den
 
     return r
+
+
+# Returns the best matches for person from the prefs dictionary.
+# Number of results and similarity function are optional params.
+
+def topMatches(prefs, person, n=5, similarity=sim_pearson):
+    scores = [(similarity(prefs, person, other), other) for other in prefs if other != person]
+
+    # Sort the list so the highest scores appear at the top
+    scores.sort();
+    scores.reverse()
+    return scores[0:n]
+
+
+# Get recommendations for a person by using a weighted average
+# of every other user's rankings
+def getRecommendations(prefs, person, similarity=sim_pearson):
+    totals = {}
+    simSums = {}
+    for other in prefs:
+        # Don't compare to yourself
+        if person == other: continue
+        sim = similarity(prefs, person, other)
+
+        # Ignore scores of zero or lower
+        if sim <= 0: continue
+        for item in prefs[other]:
+
+            # Only score unseen movies
+            if item not in prefs[person] or prefs[person][item] == 0:
+                # Similarity * Score
+                totals.setdefault(item, 0)
+                totals[item] += prefs[other][item] * sim
+                # Sum of similarities
+                simSums.setdefault(item,0)
+                simSums[item]+=sim
+
+    # Create the normalized list
+    #rankings = [(total / simSums[item], item) for item, total in totals.items()]
+    rankings = [(total / simSums[item],item) for item,total in totals.items()]
+
+    # Return the sorted list
+    rankings.sort()
+    rankings.reverse()
+    return rankings
+
+# Transform th dictionary to allow item matching products recommendations
+def dicTransformation(dictionary):
+    newDict = {}
+    for person in dictionary:
+        for item in dictionary[person]:
+            newDict.setdefault(item,{})
+            newDict[item][person] = dictionary[person][item]
+    return newDict
+
+
+
+
